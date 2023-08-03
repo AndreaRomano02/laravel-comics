8157a3b14bdf7eeb36c1ef0f07532498e2f60902
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-03 14:51:42 +0200

    Add footer bottom

--- resources/views/includes/footer/footer-bottom.blade.php
+++ resources/views/includes/footer/footer-bottom.blade.php
@@ -1,3 +1,29 @@
-<div id="footer-bottom">
-
-</div>
+  <div id="bg-bottom-foot">
+      <div class="footer-bottom container">
+          <div class="left">
+              <button>SING-UP NOW!</button>
+          </div>
+          <div class="right">
+              <a href="#">
+                  <h3>FOLLOW US</h3>
+              </a>
+              <figure>
+                  <a href="#">
+                      <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook">
+                  </a>
+                  <a href="#">
+                      <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter">
+                  </a>
+                  <a href="#">
+                      <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube">
+                  </a>
+                  <a href="#">
+                      <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest">
+                  </a>
+                  <a href="#">
+                      <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope">
+                  </a>
+              </figure>
+          </div>
+      </div>
+  </div>
