cab8e4ca39d9937e02d26305e747e6544fb2587e
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-03 16:10:36 +0200

    Add active class

--- resources/views/includes/header/header-menu.blade.php
+++ resources/views/includes/header/header-menu.blade.php
@@ -3,34 +3,54 @@
     <nav>
         <ul>
             <li>
-                <a href="{{ Route('characters') }}">CHARACTERS</a>
+                <a href="{{ Route('characters') }}" @if (Route::is('characters')) class="active" @endif>
+                    CHARACTERS
+                </a>
             </li>
             <li>
-                <a href="{{ Route('comics') }}">COMICS</a>
+                <a href="{{ Route('comics') }}" @if (Route::is('comics')) class="active" @endif>
+                    COMICS
+                </a>
             </li>
             <li>
-                <a href="{{ Route('movie') }}">MOVIES</a>
+                <a href="{{ Route('movie') }}" @if (Route::is('movie')) class="active" @endif>
+                    MOVIES
+                </a>
             </li>
             <li>
-                <a href="{{ Route('tv') }}">TV</a>
+                <a href="{{ Route('tv') }}" @if (Route::is('tv')) class="active" @endif>
+                    TV
+                </a>
             </li>
             <li>
-                <a href="{{ Route('games') }}">GAMES</a>
+                <a href="{{ Route('games') }}" @if (Route::is('games')) class="active" @endif>
+                    GAMES
+                </a>
             </li>
             <li>
-                <a href="{{ Route('collectibles') }}">COLLECTIBLES</a>
+                <a href="{{ Route('collectibles') }}" @if (Route::is('collectibles')) class="active" @endif>
+                    COLLECTIBLES
+                </a>
             </li>
             <li>
-                <a href="{{ Route('videos') }}">VIDEOS</a>
+                <a href="{{ Route('videos') }}" @if (Route::is('videos')) class="active" @endif>
+                    VIDEOS
+                </a>
             </li>
             <li>
-                <a href="{{ Route('fans') }}">FANS</a>
+                <a href="{{ Route('fans') }}" @if (Route::is('fans')) class="active" @endif>
+                    FANS
+                </a>
             </li>
             <li>
-                <a href="{{ Route('news') }}">NEWS</a>
+                <a href="{{ Route('news') }}" @if (Route::is('news')) class="active" @endif>
+                    NEWS
+                </a>
             </li>
             <li>
-                <a href="{{ Route('shop') }}">SHOP</a>
+                <a href="{{ Route('shop') }}" @if (Route::is('shop')) class="active" @endif>
+                    SHOP
+                </a>
             </li>
         </ul>
     </nav>
