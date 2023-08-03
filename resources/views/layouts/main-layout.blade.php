5a474f2919e32e24a0d65a522dc7bd848efaca96
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-03 13:37:09 +0200

    Add genral preparative

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Comics | @yield('page-title')</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <h1>test</h1>
+</body>
+
+</html>
