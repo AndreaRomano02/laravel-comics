ee728b6b384a6b2a51423052b5603c834a46d541
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-03 15:17:56 +0200

    Add comics web page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,6 +1,26 @@
+@php
+    $comics = config('comics');
+@endphp
+
 @extends('layouts.main-layout')
 @section('main-content')
-    <div class="homepage">
-        <h1>COMICS</h1>
-    </div>
+    <section id="comics">
+        <div class="container card-container">
+            <div class="current">CURRENT SERIES</div>
+
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <figure>
+                        <div class="img-container">
+                            <img src="{{ $comic['thumb'] }}" alt="game">
+                        </div>
+                        <figcaption>{{ $comic['series'] }}</figcaption>
+                    </figure>
+                </div>
+            @endforeach
+        </div>
+        <div class="button">
+            <button>Load more...</button>
+        </div>
+    </section>
 @endsection
